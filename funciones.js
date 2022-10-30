0066fe767935929792c21d5e67dc6baa51bc2810
starlin <sistemcabral.27@gmail.com>
2022-10-30 14:37:59 -0400

    com

--- funciones.js
+++ funciones.js
@@ -4,27 +4,15 @@
 btn=document.querySelector('.btn__menu')
 buscador=document.querySelector('.form');
 menu=document.querySelector('.navegacion');
-logo=document.querySelector('.lo');
-
-Habilidades=document.querySelector('.habilidades')
 
 
 
 
-function EVENmenu (){
+
 btn.addEventListener('click',(e)=>{
 
 menu.classList.toggle('show')
 
 })
-}
-EVENmenu()
 
-lupa=document.querySelector('.lupa')
-buscador=document.querySelector('#search')
-lupa.addEventListener('click',()=>{
-   
- if(buscador.hidden==false)buscador.hidden=true; 
- else buscador.hidden=false
-})
 
